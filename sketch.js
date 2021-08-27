e441a22e0c8a31d6e6ec8dca3fdf47d054212308
Bobby2006 <89638487+Bobby2006@users.noreply.github.com>
2021-08-27 20:30:44 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+
+  box=createSprite(200,200,50,50);
+
+}
+
+function draw() 
+{
+  background("Blue");
+
+  if(keyDown(LEFT_ARROW))
+  {
+    box.x=box.x-2;
+  }
+  
+  if(keyDown(RIGHT_ARROW))
+  {
+    box.x=box.x+2;
+  }
+  
+  if(keyDown(UP_ARROW))
+  {
+    box.y=box.y-2;
+  }
+  
+  if(keyDown(DOWN_ARROW))
+  {
+    box.y=box.y+2;
+  }
+
+  drawSprites();
+}
+
+
+
+
